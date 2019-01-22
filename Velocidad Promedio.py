ee127afb70aaff414867cc9be9160430a5b7eb96
SofiaDaniela <46821612+SofiaDaniela@users.noreply.github.com>
2019-01-22 11:04:28 -0600

    Sofía Daniela Méndez Sandoval, Grupo 03

---
+++ Velocidad Promedio.py
@@ -0,0 +1,12 @@
+# Autor: Sofía Daniela Méndez
+# Calcula velocidad promedio, dado el tiempo y la distancia
+
+distancia = int(input("Teclea la distancia de tu viaje en km enteros: "))
+tiempo = int(input("Teclea el tiempo de tu viaje en hrs. enteras: "))
+
+velocidad = distancia / tiempo
+
+print("Velocidad promedio: ", velocidad, "km/h")
+
+
+
